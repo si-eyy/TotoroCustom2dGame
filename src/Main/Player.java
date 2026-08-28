396d67805a21ae1dc8216be5b790444a497771f7
andi-scriptdev <andreavalenzuela1029@gmail.com>
2026-08-29 05:05:58 +0800

    Add sprite animation and update logic to Player class; enhance movement handling

--- src/Main/Player.java
+++ src/Main/Player.java
@@ -12,6 +12,9 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+    private int spriteCounter = 0;
+    private int spriteNum = 1;
+
     public Player(GamePanel gp, KeyHandler keyH) {
 
         this.gp = gp;
@@ -61,6 +64,8 @@ public class Player extends Entity {
 
     public void update () {
 
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
+
         if (keyH.upPressed == true) {
             direction = "up";
             y -= speed;
@@ -77,7 +82,18 @@ public class Player extends Entity {
             direction = "right";
             x += speed;
         }
+    }
 
+        spriteCounter++;
+        if (spriteCounter > 12) { 
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            } 
+            else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
 
     public void draw(Graphics2D g2) {
@@ -89,13 +105,36 @@ public class Player extends Entity {
 
         switch (direction) {
             case "up":
-                image = up1;
+                if (SpriteNum == 1) {
+                    image = up1;
+                }
+                if (SpriteNum == 2) {
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down1;
+                if (SpriteNum == 1) {
+                    image = down1;
+                } 
+                if (SpriteNum == 2) {
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if (SpriteNum == 1) {
+                    image = left1;
+                }
+                if (SpriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if (SpriteNum == 1) {
+                    image = right1;
+                }
+                if (SpriteNum == 2) {
+                    image = left2;
+                }
                 break;
             case "right":
                 image = right1;
